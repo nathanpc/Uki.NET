0b37f167a5632f6c2640135fce2a5d3d2c08b6d1
Nathan Campos <eeepc904@gmail.com>
2020-07-12 20:36:16 +0100

    Testing the workspace

--- Uki.NET/Program.cs
+++ Uki.NET/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Diagnostics;
+using Uki;
 
 namespace Uki.NET {
 	static class Program {
@@ -9,6 +11,11 @@ namespace Uki.NET {
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			// Open a new workspace and test it out.
+			Workspace workspace = new Workspace(@"C:\Documents and Settings\Administrator\My Documents\TestUki");
+			Debug.Print(workspace.ToString());
+			
+			// Setup and run the main application form.
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());

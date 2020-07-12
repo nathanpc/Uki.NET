a82b6d15bf0e18ec58f6d50a94a6bb1c85ad0745
Nathan Campos <eeepc904@gmail.com>
2020-07-12 16:03:36 +0100

    Added some Uki constants

---
+++ Uki/Constants.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uki {
+	public static class Constants {
+		// Files
+		public const string MANIFEST_PATH  = "MANIFEST.uki";
+		public const string VARIABLES_PATH = "VARIABLES.uki";
+
+		// Paths
+		public const string ARTICLE_ROOT  = "pages";
+		public const string TEMPLATE_ROOT = "templates";
+		public const string ASSETS_ROOT   = "assets";
+
+		// Extensions
+		public const string ARTICLE_EXTENSION  = "htm";
+		public const string TEMPLATE_EXTENSION = "htm";
+	}
+}

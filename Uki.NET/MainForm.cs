7613852ea04a950a788bce8abbe8330479d1bd58
Nathan Campos <eeepc904@gmail.com>
2020-07-13 12:21:36 +0100

    Populating the TreeView with articles and templates

--- Uki.NET/MainForm.cs
+++ Uki.NET/MainForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.IO;
 
 namespace Uki.NET {
 	public partial class MainForm : Form {
@@ -21,6 +22,60 @@ namespace Uki.NET {
 			
 			// Initialize and display window components.
 			InitializeComponent();
+			PopulateTreeView();
+		}
+
+		/// <summary>
+		/// Populates the TreeView with articles and templates.
+		/// </summary>
+		private void PopulateTreeView() {
+			List<string> paths = new List<string>();
+
+			// Populate the paths list with articles.
+			foreach (Page page in workspace.Articles) {
+				paths.Add("Workspace" + Path.DirectorySeparatorChar + "Articles" +
+					page.Folder + page.ToString());
+			}
+
+			// Populate the paths list with templates.
+			foreach (Page page in workspace.Templates) {
+				paths.Add("Workspace" + Path.DirectorySeparatorChar + "Templates" +
+					page.Folder + page.ToString());
+			}
+
+			// Populate the TreeView and expand the tree nodes.
+			PopulateTreeViewFromPaths(treeView, paths, Path.DirectorySeparatorChar);
+			treeView.ExpandAll();
+		}
+
+		/// <summary>
+		/// Populates a TreeView from a list of paths.
+		/// </summary>
+		/// <see cref="https://stackoverflow.com/a/1768414/126353" />
+		/// <param name="treeView">TreeView to be populated.</param>
+		/// <param name="paths">Paths to populate with.</param>
+		/// <param name="pathSeparator">Path separator character.</param>
+		private static void PopulateTreeViewFromPaths(TreeView treeView, IEnumerable<string> paths, char pathSeparator) {
+			TreeNode lastNode = null;
+			string subPathAgg;
+
+			foreach (string path in paths) {
+				subPathAgg = string.Empty;
+				foreach (string subPath in path.Split(pathSeparator)) {
+					subPathAgg += subPath + pathSeparator;
+					TreeNode[] nodes = treeView.Nodes.Find(subPathAgg, true);
+
+					if (nodes.Length == 0) {
+						if (lastNode == null) {
+							lastNode = treeView.Nodes.Add(subPathAgg, subPath);
+						} else {
+							lastNode = lastNode.Nodes.Add(subPathAgg, subPath);
+						}
+					} else {
+						lastNode = nodes[0];
+					}
+				}
+			}
 		}
 	}
 }
